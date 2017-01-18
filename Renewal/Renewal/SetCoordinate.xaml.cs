1c6a4da093bc69477822ee53d162027d524ac4d2
Seongeun <21400026@handong.edu>
2017-01-18 19:48:56 +0900

    no message

--- Renewal/Renewal/SetCoordinate.xaml.cs
+++ Renewal/Renewal/SetCoordinate.xaml.cs
@@ -18,6 +18,7 @@ namespace Renewal
     /// <summary>
     /// SetCoordinate.xaml에 대한 상호 작용 논리
     /// </summary>
+    ///
     public partial class SetCoordinate : Window
     {
         public SetCoordinate()
